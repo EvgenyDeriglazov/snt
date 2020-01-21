bd3ccbb335426d99b8fb0942213660016af6fe37
Evgeny Deriglazov <evgenyderiglazov@yandex.ru>
2020-01-21 12:24:23 +0300

    Electric Meter model test finilized.

--- data/tests/test_models.py
+++ data/tests/test_models.py
@@ -1,13 +1,14 @@
-# -- Collectstatic (ValueError: Missing staticfiles manifest entry...) --
+# --- Collectstatic (ValueError: Missing staticfiles manifest entry...) ---
 # python3 manage.py collectstatic
-
-# -- Verbosity --
+# --- Verbosity ---
 # python3 manage.py test --verbosity (1-default, 0, 1, 2, 3)
-
-# -- Run specific modules --
+# --- Run specific test modules ---
 # python3 manage.py test data.tests
 # python3 manage.py test data.tests.test_models
 # python3 manage.py test data.tests.test_models.TestClass
+# --- Coverage.py --- 
+# coverage run --source='.' manage.py test <appname>
+# coverage report
 
 from django.test import TestCase
 from data.models import *
@@ -418,4 +419,30 @@ class ElectricMeterModelTest(TestCase):
         self.assertEquals(defaults, 'T1')
         self.assertEquals(help_text, 'Тип прибора учета')
         self.assertEquals(obj.model_type, 'T2')
+
+    def test_acceptance_date_field(self):
+        obj = ElectricMeter.objects.get(id=1)
+        field_label = obj._meta.get_field('acceptance_date').verbose_name
+        is_blank = obj._meta.get_field('acceptance_date').blank
+        is_null = obj._meta.get_field('acceptance_date').null
+        self.assertEquals(field_label, 'дата установки')
+        self.assertEquals(is_blank, True)
+        self.assertEquals(is_null, True)
+        self.assertEquals(obj.acceptance_date, None)
+
+    def test_object_name(self):
+        obj = ElectricMeter.objects.get(id=1)
+        obj_name = f'{obj.model}' 
+        self.assertEquals(obj_name, obj.__str__())
+        # or self.assertEquals(object_name, str(lp_obj))
+    
+    def test_get_absolute_url(self):
+        obj = ElectricMeter.objects.get(id=1)
+        self.assertEquals(obj.get_absolute_url(), '/data/electric-meter-detail/1')
+
+    def test_verbose_names(self):
+        self.assertEquals(ElectricMeter._meta.verbose_name, 'прибор учета электроэнергии')
+        self.assertEquals(ElectricMeter._meta.verbose_name_plural, 'приборы учета электроэнергии')
+
+
 
