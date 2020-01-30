44777a897b9c38375f73492e8f842e858afa8281
Evgeny Deriglazov <evgenyderiglazov@yandex.ru>
2020-01-30 21:17:27 +0300

    First draft view and url conf.

--- data/views.py
+++ data/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+import datetime
 
 # Create your views here.
+def homepage(request):
+    now = datetime.datetime.now()
+    now = now.strftime("%d/%m/%Y")
+    html = "<html><body>It is now %s.</body></html>" %now
+    return HttpResponse(html)
--- snt/urls.py
+++ snt/urls.py
@@ -19,6 +19,7 @@ from django.conf.urls.static import static
 from django.conf import settings
 
 urlpatterns = [
+    path('', include('data.urls')),
     path('admin/', admin.site.urls),
     path('data/', include('data.urls')),
 ]
