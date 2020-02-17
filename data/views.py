4012eb41e52386bfe82c90b100670c6bf303a81a
Evgeny Deriglazov <evgenyderiglazov@yandex.ru>
2020-02-17 21:32:48 +0300

    Error handling for user_new_payment_view(): Incorrect electric counter type, LandPlot.DoesNotExist, ValidationErrors form ElectricityPayments save() method, LandPlot does not belong to current user.

--- data/views.py
+++ data/views.py
@@ -23,6 +23,10 @@ def homepage(request):
     }
     
     return render(request, 'index.html', context=context)
+@login_required
+def error_view(request):
+    """View for draft error page."""
+    render(request, 'error_page.html', context=context)
 
 @login_required
 def user_payments_view(request):
@@ -143,14 +147,24 @@ def user_new_payment_view(request, plot_num):
     """View function to display form for new electricity payment
     record."""
     current_user = request.user
-    land_plot = LandPlot.objects.get(plot_number=plot_num)
+    # Get requested land plot instance form db or return error page
+    try:
+        land_plot = LandPlot.objects.get(plot_number=plot_num)
+    except LandPlot.DoesNotExist:
+        error_message = "Участка номер {} нет в базе данных.".format(plot_num)
+        context = {
+            'error_message': error_message,
+            }
+        return render(request, 'error_page.html', context=context)
+    # Get electrical counter model type and model type description
     el_counter_type = land_plot.electrical_counter.model_type
     el_counter_type_disp = land_plot.electrical_counter.get_model_type_display()
+    # If user tries to POST data
     if request.method == 'POST' and land_plot.user == current_user:
         # Instantiate form for T1 electric counter type with user data
         if el_counter_type == 'T1':
             form = T1NewElectricityPaymentForm(request.POST)
-            # Validate T1 form data 
+            # Validate T1 form data and try to save to db 
             if form.is_valid():
                 t1_new_cleaned = form.cleaned_data['t1_new']
                 try:
@@ -158,8 +172,11 @@ def user_new_payment_view(request, plot_num):
                         plot_number=land_plot,
                         t1_new=t1_new_cleaned,
                         )
+                    # Calculate payment for new record
                     new_record.calculate_payment()
                 except ValidationError as validation_error:
+                    # ValidationError from save() method
+                    # Prepare error message and context
                     error_message_list = validation_error.message.split("\n")
                     context = {
                         'plot_number': plot_num,
@@ -167,9 +184,9 @@ def user_new_payment_view(request, plot_num):
                         'error_message_list': error_message_list,
                         'el_counter_type_disp': el_counter_type_disp,
                         }
-                    # Error - render form with error message
+                    # Render form with error message
                     return render(request, 'new_payment.html', context=context)
-                # OK - redirect to plot electricity payments list page
+                # Redirect to plot electricity payments list page
                 return HttpResponseRedirect(
                     reverse('plot-electricity-payments', args=[plot_num])
                     ) 
@@ -177,7 +194,7 @@ def user_new_payment_view(request, plot_num):
         # Instantiate form for T2 electric counter type with user data
         else: 
             form = T2NewElectricityPaymentForm(request.POST) 
-            # Validate T2 form data
+            # Validate T2 form data and try to save to db
             if form.is_valid():
                 t1_new_cleaned = form.cleaned_data['t1_new']
                 t2_new_cleaned = form.cleaned_data['t2_new']
@@ -187,9 +204,11 @@ def user_new_payment_view(request, plot_num):
                         t1_new=t1_new_cleaned,
                         t2_new=t2_new_cleaned,
                         )
+                    # Calculate payment for new record
                     new_record.calculate_payment()
-                # Get ValidationError from save() method
                 except ValidationError as validation_error:
+                    # ValidationError from save() method
+                    # Prepare error message and context
                     error_message_list = validation_error.message.split("\n")
                     context = {
                         'plot_number': plot_num,
@@ -197,9 +216,9 @@ def user_new_payment_view(request, plot_num):
                         'error_message_list': error_message_list,
                         'el_counter_type_disp': el_counter_type_disp,
                         }
-                    # Error - render form with error message
+                    # Render form with error message
                     return render(request, 'new_payment.html', context=context)
-                # OK - redirect to plot electricity payments list page
+                # Redirect to plot electricity payments list page
                 return HttpResponseRedirect(
                     reverse('plot-electricity-payments', args=[plot_num])
                     )
@@ -209,19 +228,21 @@ def user_new_payment_view(request, plot_num):
             form = T1NewElectricityPaymentForm()
         elif el_counter_type == 'T2' and land_plot.user == current_user:
             form = T2NewElectricityPaymentForm()
-        # Error - if incorrect counter type
+        # If counter type is not correct render error page
         elif el_counter_type != 'T1' and el_counter_type != 'T2':
             error_message_list = "Неверный тип счетчика."
             context = {
                 'error_message': error_message,
                 }
+            return render(request, 'error_page.html', context=context)
 
-        # Error - if incorrect user
-        else:
-            error_message = "У вас нет такого номер участка."
+        # If land plot does not belong to current user render error page
+        elif land_plot.user != current_user:
+            error_message = "У вас нет участка с номером {}.".format(plot_num)
             context = {
                 'error_message': error_message,
                 }
+            return render(request, 'error_page.html', context=context)        
 
     # Context for empty form
     context = {
